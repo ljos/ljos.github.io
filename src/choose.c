da6393efc25faf340cacc90ed721ecaa2a074d5e
Bjarte Johansen <bjarte.johansen@gmail.com>
2014-04-11 22:07:45 +0200

    file is a bit different after adding references

--- src/choose.c
+++ src/choose.c
@@ -2,39 +2,35 @@
 #include <SWI-Prolog.h>
 #include <gmp.h>
 
-static foreign_t
-pl_choose(term_t size, term_t take, term_t result)
+static foreign_t                                                    
+pl_choose(term_t size, term_t take, term_t result)                   
 {
+  mpz_t rop;                                                         
+  mpz_t n;
+  unsigned long int k;
+  int rc;
 
-mpz_t rop;
-mpz_t n;
-unsigned long int k;
-int rc;
+  mpz_init(rop);                                                     
+  mpz_init(n);
 
-mpz_init(rop);
-mpz_init(n);
+  if ( PL_get_mpz(take, rop) && PL_get_mpz(size, n) )                
+    {
+      k = mpz_get_ui(rop);                                             
+      mpz_bin_ui(rop, n, k);                                     
+      rc = PL_unify_mpz(result, rop);                               
+    }
+  else
+    {
+      rc = FALSE;                                                    
+    }
 
-if ( PL_get_mpz(take, rop) && PL_get_mpz(size, n) )
-  {
-
-k = mpz_get_ui(rop);
-mpz_bin_ui(rop, n, k);
-rc = PL_unify_mpz(result, rop);
-
-}
-else
-  {
-    rc = FALSE;
-  }
-
-mpz_clear(rop);
-mpz_clear(n);
-
-return rc;
+  mpz_clear(rop);                                                   
+  mpz_clear(n);
+  return rc;                                                       
 }
 
 install_t
-install_choose()
+install_choose()                                                  
 {
   PL_register_foreign("choose", 3, pl_choose, 0);
 }
